b40db41bce9816ca8abed64c7a7b2a0a5a190be2
Comrade X <106185020+comrade09@users.noreply.github.com>
2024-01-07 22:56:06 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,7 +4,7 @@ from urllib.parse import quote
 from pyrogram import Client, emoji, filters
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
-from pyrogram.enum import ParseMode
+from pyrogram.enums import ParseMode
 
 from utils import get_search_results
 from info import CACHE_TIME, SHARE_BUTTON_TEXT, AUTH_USERS, AUTH_CHANNEL
