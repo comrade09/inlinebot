9ca3a81f19e5c3947924258d3706e484d8862637
Comrade X <106185020+comrade09@users.noreply.github.com>
2024-01-07 21:25:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
+            name="Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
