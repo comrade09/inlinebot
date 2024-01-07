fda1dc1118a56b630b38a6fb9567862d987f0aa2
Comrade X <106185020+comrade09@users.noreply.github.com>
2024-01-07 21:17:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,19 @@ import logging.config
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from utils import Media
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
-
+import pyromod.listen
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
+            session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
